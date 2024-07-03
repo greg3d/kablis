e0711ff2b5e43c1c0810a5863a77e4f21460e42a
grosenbaum <grosenbaum@chegg.com>
2024-07-03 16:42:27 +0300

    pre modx

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import "aos/dist/aos.css"
+
 import "./fonts.scss"
-import "bulma/bulma.scss"
+import "bulma/css/bulma.min.css"
 import "./main.scss"
 
 // import Swiper JS
@@ -10,21 +10,24 @@ import {Autoplay, EffectFade} from 'swiper/modules'
 import 'swiper/css';
 import 'swiper/css/autoplay';
 import 'swiper/css/effect-fade';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 document.addEventListener("DOMContentLoaded", () => {
     setBurger()
     stickyMenu()
-    mainCarousel()
+    carousels()
     effects()
 })
 
 const effects = () => {
     new Rellax('.parallaxed');
+    AOS.init();
 }
 
-const mainCarousel = () => {
-    const swiper = new Swiper('.carousel', {
+const carousels = () => {
+    new Swiper('.carousel', {
         loop: true,
         slidesPerView: 1,
         spaceBetween: 0,
@@ -34,11 +37,28 @@ const mainCarousel = () => {
         effect: "fade",
         speed: 5000,
         autoplay: {
-            delay: 4000,
+            delay: 3000,
             pauseOnMouseEnter: false,
             disableOnInteraction: false,
         }
     });
+
+    new Swiper('.swiper', {
+        loop: true,
+        slidesPerView: 1,
+        autoHeight: true,
+        spaceBetween: 10,
+        modules: [Autoplay],
+        speed: 1500,
+        effect: "slide",
+        pagination: false,
+        navigation: false,
+        autoplay: {
+            delay: 7000,
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+        }
+    })
 }
 
 const setBurger = () => {
